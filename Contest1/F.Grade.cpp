9d4c1d384b2b32a37ca830645a8f5f2039d0ee3f
sara-mohamd <saraamohamed0038@gmail.com>
2025-03-03 21:29:53 +0200

    upsolve problem F

--- Contest1/F.Grade.cpp
+++ Contest1/F.Grade.cpp
@@ -8,32 +8,14 @@ int main()
   while (t--)
   {
     int n, m;
-    long long sum = 0;
     cin >> n >> m;
-    int a[n] = {0};
-    long long idx = n, x = INT_MAX, ans = 0;
-    for (int i = 0; i < n; i++)
+    int a[n + 1] = {0};
+    for (int i = 1; i <= n; i++)
     {
       cin >> a[i];
-      if (sum > m)
-        continue;
-      sum += a[i];
-      idx = i + 1;
-      if (sum == m)
-      {
-        cout << m << endl;
-        goto end;
-      }
+      int mn = min(m - a[0], a[i]);
+      a[0] += mn;
     }
-    for (int i = 0; i < idx; i++)
-    {
-      if ((m - (sum - a[i])) < x)
-      {
-        ans = (sum - a[i]);
-        x = (m - (sum - a[i]));
-      }
-    }
-    cout << ans << endl;
-  end:;
+    cout << a[0] << endl;
   }
 }
